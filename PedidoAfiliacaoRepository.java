bf1514a50b6fc76f6836d03c34f13496659a6cc6
Marcelo Prass <marcelocambe2005@gmail.com>
2025-11-20 21:35:27 -0300

    Delete PedidoAfiliacaoRepository.java

--- PedidoAfiliacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.projeto.redemaissocial.repository;
-
-import br.projeto.redemaissocial.domain.PedidoAfiliacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoAfiliacaoRepository extends JpaRepository<PedidoAfiliacao, Long> {
-}
